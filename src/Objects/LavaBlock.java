ca493e5f7339be96fd4c168eecf928c9f66a9a5b
stef2805 <122122338+stef2805@users.noreply.github.com>
2023-05-16 17:09:54 +0300

    adaugat factory

--- src/Objects/LavaBlock.java
+++ src/Objects/LavaBlock.java
@@ -17,9 +17,9 @@ public class LavaBlock extends GameObject {
     {
         super(x,y,id);
         this.type = type;
-        if(type.compareTo("animated")==0)
+        if(type.compareTo("animated") == 0)             //verific daca este bloc neanimat de lava (bloc de deasupra)
             lavaMoving = new Animation(5,tex.lavaBlock[0],tex.lavaBlock[1],tex.lavaBlock[2]);
-        else if (type.compareTo("static")!=0)
+        else if (type.compareTo("static")!=0)           //daca nu este nici bloc animat nici static, inseamna ca nu a fost param dat bine
         {
             System.out.println("parametru la lavaBlock dat prost");
         }
@@ -29,16 +29,16 @@ public class LavaBlock extends GameObject {
     public void tick()
     {
         if(type == "animated")
-            lavaMoving.runAnimatin();
+            lavaMoving.runAnimatin();                   //daca este animat rulez animatia
     }
 
     @Override
     public void render(Graphics g)
     {
         if(type.equals("animated"))
-            lavaMoving.drawAnimation(g,(int)x,(int)y,32,32);
+            lavaMoving.drawAnimation(g,(int)x,(int)y,32,32);        //daca e animat desenez o animatie
         else
-            g.drawImage(tex.lavaBlock[4],(int)x,(int)y,null);
+            g.drawImage(tex.lavaBlock[4],(int)x,(int)y,null);            //daca nu e animat desenez un bloc static
     }
 
     @Override
